d8af6906e5e6d3f6516a95c3e812399d82d5b3c2
ReyanshS11 <reyansh.saindane@gmail.com>
2026-01-17 18:11:37 -0500

    save

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 from mini_torch.core.tensor import Tensor
 from mini_torch.core.ops import max
-from mini_torch.nn.layers import Linear, ReLU, Conv1d, Conv2d
+from mini_torch.nn.layers import Linear, ReLU, Conv1d, Conv2d, Conv3d
 from mini_torch.nn.loss import MSELoss
 from mini_torch.optim.sgd import SGD
 from mini_torch.data.dataset import Dataset
@@ -11,10 +11,9 @@ import torch.nn as nn
 import numpy as np
 import pandas as pd
 
-x = Tensor(np.random.randn(4, 3, 10, 10), requires_grad=True)
-#x = torch.randn((4, 3, 10, 10), requires_grad=True)
+x = Tensor(np.random.randn(4, 3, 10, 10, 10), requires_grad=True)
 
-m = Conv2d(3, 1, (3, 5), 2)
+m = Conv3d(3, 1, (3, 3, 5), 2)
 m = m(x)
 
 loss = m.sum()
